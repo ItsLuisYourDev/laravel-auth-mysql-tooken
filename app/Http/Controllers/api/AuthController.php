f16b38d7bb144cf8088dd5cb1d136bbc6f46250c
luisbsc <antocede5@gmail.com>
2024-10-12 22:32:32 -0500

    se creo el metodo login para loguear usuario meditante la tabla user de la bd

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -4,11 +4,30 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\User;
 
 class AuthController extends Controller
 {
     //
     public function login(Request $request) {
-        echo($request -> name);
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json(['message' => 'Credenciales inválidas'], 401);
+        }
+
+        // Crear un token para el usuario
+        // $token = $user->createToken('API Token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => 'mitoken',
+        ]);
     }
 }
