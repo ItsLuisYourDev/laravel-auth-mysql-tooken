bb95b52a7baa2a0415b644405641f40916a97bf8
luisbsc <antocede5@gmail.com>
2024-10-13 18:18:29 -0500

    se realaciono las tablas usuario, con rol meidante la tabla rol de usuario, se creo el modelo role, el cual maneja la relacion entre las tablas

--- app/Http/Controllers/EduFinancieraController.php
+++ app/Http/Controllers/EduFinancieraController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Role;
 
 class EduFinancieraController extends Controller
 {
     //
     public function index(){
-        echo('hola desde edu');
+        echo('eres admin');
+        $rol = Role::find(1);
+        echo($rol);
     }
 }
