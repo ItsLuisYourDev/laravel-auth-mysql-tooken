28599f6f093298b15b8ef4cfc637a0792e505c1f
luisbsc <antocede5@gmail.com>
2024-10-13 18:57:16 -0500

    se corrigo la relacion en el modelo usaurio soblre el usuario y el rol

--- app/Http/Controllers/EduFinancieraController.php
+++ app/Http/Controllers/EduFinancieraController.php
@@ -4,13 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Role;
+use App\User;
+use Illuminate\Support\Facades\Auth;
 
 class EduFinancieraController extends Controller
 {
     //
     public function index(){
         echo('eres admin');
-        $rol = Role::find(1);
-        echo($rol);
+        // $rol = Role::find(1);
+        // echo($rol);
+        // Obtener los roles asociados con un usuario
+        // $role = Role::find(1); // Reemplaza 1 con el id del rol que quieras probar
+        // $users = $role->users;
+             // Obtener el usuario autenticado
+
+        $user = User::find(1); // Reemplaza 1 con el id del usuario que quieras probar
+        $roles = $user->roles;
+        echo($user);
     }
 }
--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Role;
 
 class TestController extends Controller
 {
     //
     public function index(){
+        $role = Role::find(1);
+        $users = $role->users;
+        echo($role);
+        echo($users);
         $list = [
             'nombre' => 'luis'
         ];
