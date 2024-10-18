3921b25c924140d316a8ac02c7cf87d871f3160a
luisbsc <antocede5@gmail.com>
2024-10-18 17:07:26 -0500

    creacion un campo para controlar el itmpo del token

--- app/Http/Middleware/ExtendSanctumToken.php
+++ app/Http/Middleware/ExtendSanctumToken.php
@@ -3,6 +3,9 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\Request;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
 
 class ExtendSanctumToken
 {
@@ -13,8 +16,22 @@ class ExtendSanctumToken
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next)
     {
+        // Si el usuario está autenticado
+if (Auth::user()) {
+    // Obtén el token actual
+    $token = Auth::user()->currentAccessToken();
+
+    // Verifica si el token es válido y si hay interacción reciente
+    if ($token) {
+        // Extiende el token otros 5 minutos
+        $token->forceFill([
+            'expires_at' => Carbon::now()->addMinutes(5)
+        ])->save();
+    }
+}
         return $next($request);
+
     }
 }
